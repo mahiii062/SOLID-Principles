c3ce8d9da3749eb8e30a973234d6df809b2c1d74
mahiii062 <mahi.sust62@gmail.com>
2026-06-15 01:14:47 +0600

    ISP

---
+++ ISP.java
@@ -0,0 +1,118 @@
+/*
+Let's take an Interface:
+
+interface Worker {
+    void work();
+    void eat();
+    void sleep();
+}
+
+Human & Robot both are Worker।
+>>>>Human
+class HumanWorker implements Worker {
+
+    public void work() {
+        System.out.println("Working");
+    }
+
+    public void eat() {
+        System.out.println("Eating");
+    }
+
+    public void sleep() {
+        System.out.println("Sleeping");
+    }
+}
+>>>>Robot
+class RobotWorker implements Worker {
+
+    public void work() {
+        System.out.println("Working");
+    }
+
+    public void eat() {
+        // Nothing
+    }
+
+    public void sleep() {
+        // Nothing
+    }
+}
+
+Problem
+Robot:
+
+Can Work?   YES
+Can Eat?    NO
+Can Sleep?  NO
+
+But robot has to implement
+eat()
+sleep()
+
+No matter what, it has to implement those methods,
+even if it does nothing.
+This ISP Violation।
+*/
+
+/*
+ISP solution:
+break down large interface into smaller ones.
+*/
+
+// Worker interface
+interface Workable {
+    void work();
+}
+
+// Eatable interface
+interface Eatable {
+    void eat();
+}
+
+// Sleepable interface
+interface Sleepable {
+    void sleep();
+}
+
+// Human
+class HumanWorker
+        implements Workable,
+        Eatable,
+        Sleepable {
+
+    public void work() {
+        System.out.println("HumanWorking");
+    }
+
+    public void eat() {
+        System.out.println("HumanEating");
+    }
+
+    public void sleep() {
+        System.out.println("Human Sleeping");
+    }
+}
+
+// Robot
+class RobotWorker
+        implements Workable {
+
+    public void work() {
+        System.out.println("Robot Working");
+    }
+}
+
+// main class
+public class ISP {
+    public static void main(String[] args) {
+
+        HumanWorker human = new HumanWorker();
+        RobotWorker robot = new RobotWorker();
+
+        human.work();
+        human.eat();
+        human.sleep();
+        robot.work();
+    }
+}
